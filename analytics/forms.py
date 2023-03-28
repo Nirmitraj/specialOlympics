6b88c019b676ff0d91f8ab41d3c1903b2906232e
krishnateja <krishnateja.kaspe001@umb.edu>
2023-03-28 15:35:00 -0400

    space adjustments

--- analytics/forms.py
+++ analytics/forms.py
@@ -1,24 +1,13 @@
 from django import forms
 from analytics.models import SchoolDetails
 
-#class StateForm(forms.Form):
-#    state_abv = forms.CharField(label='state_abv', max_length=6,initial='ma')
-
-
-class StateForm(forms.Form):
-    class Meta:
-        model = SchoolDetails
-        fields=('school_state')
-        #SchoolDetails.objects.values_list('school_state').distinct())
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields['school_state'] = SchoolDetails.objects.none()
-
-
-
-STATE_CHOICES= list(
+STATE_CHOICES_RAW= list(
 SchoolDetails.objects.values_list('state_abv','school_state').distinct())
+STATE_CHOICES = []
 
+for val  in STATE_CHOICES_RAW:
+    if val[0]!='-99':
+        STATE_CHOICES.append(val)
+        
 class StateForm(forms.Form):
     state_abv= forms.CharField(label='which state data do you want to visualize?', widget=forms.Select(choices=STATE_CHOICES))
--- analytics/models.py
+++ analytics/models.py
@@ -1,25 +1,5 @@
 from django.db import models
 
-#Create your models here.
-# class SchoolInfo(models.Model): ## dummy table
-#     school_ID=models.CharField(max_length=90,null=False,default=None)
-#     school_name=models.CharField(max_length=255,null=True,default=None)
-#     school_state=models.CharField(max_length=55,null=True,default=None)
-#     school_county=models.CharField(max_length=120,null=True,default=None)
-#     state_abv=models.CharField(max_length=6,null=False,default=None)
-#     zipcode=models.IntegerField(null=False,default=None)
-#     survey_taken=models.BooleanField(null=True,default=None)
-#     gradeLevel_WithPreschool=models.CharField(max_length=120,null=True,default=None)
-#     locale_number=models.IntegerField(null=True,default=None)
-#     locale=models.CharField(max_length=32,null=True,default=None)
-#     student_enrollment_range=models.CharField(max_length=56,null=True,default=None)
-#     student_free_reduced_lunch=models.CharField(max_length=56,null=True,default=None)
-#     student_nonwhite_population=models.CharField(max_length=56,null=True,default=None)
-#     implementation_level=models.CharField(max_length=120,null=True,default=None)
-#     unified_sports_component=models.BooleanField(null=False,default=None)
-#     youth_leadership_component=models.BooleanField(null=False,default=None)
-#     whole_school_component=models.BooleanField(null=True,default=None)
-#     survey_taken_year=models.IntegerField(null=False,default=None)
 
 class SchoolDetails(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
@@ -45,15 +25,3 @@ class SchoolDetails(models.Model):
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-
-# create table analytics_SchoolDetails
-#  (
-#   PersonID int,
-#   LastName varchar(255),
-#   FirstName varchar(255),
-#   Address varchar(255),
-#   City varchar(255),
-#   City2 varchar(255), 
-#   City3 varchar(255), 
-#   City4 varchar(255)
-# );
