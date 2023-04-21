5bc83a3c8d43d641fec3d057fd0955e8c30a8732
krishnateja <krishnateja.kaspe001@umb.edu>
2023-04-21 19:21:24 -0400

    year filter name change

--- analytics/forms.py
+++ analytics/forms.py
@@ -18,7 +18,16 @@ implementationlevel = {'all':'all',
 
 locale = {'all':'all','City':'city','Suburb':'suburb','Rural':'rural'}
 
-years = dict(SchoolDetails.objects.values_list('survey_taken_year','survey_taken_year').distinct())
+years = SchoolDetails.objects.values_list('survey_taken_year',flat=True).distinct()
+'''
+in company terms 2022 is equal to 14 and the data they started collecting is from 2008
+similarly 2021 is equal to 13
+logic below is to convert actual year to smaller number (14,13,12...)
+so any year - 2008 gives that years smaller value
+ex: 2022-2008 =14 
+'''
+year_dict = {val:'year '+str(val-2008)+' '+str(val) for val in years if type(val) == int and val > 2008 }
+
 
 schoollevels = {'all':'all','Elementary':'Elementary','High':'High','Middle':'Middle','Preschool':'Preschool'}
 class Filters(forms.Form):
@@ -26,4 +35,4 @@ class Filters(forms.Form):
     implementation_level = forms.CharField(label='Implementation level', widget=forms.Select(choices=implementationlevel.items(),attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
     locale__startswith = forms.CharField(label='Locale', widget=forms.Select(choices=locale.items(),attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
     gradeLevel_WithPreschool = forms.CharField(label='School level',widget = forms.Select(choices = schoollevels.items(),attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
-    survey_taken_year= forms.CharField(label='Survey year', widget=forms.Select(choices=years.items(),attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    survey_taken_year= forms.CharField(label='Survey year', widget=forms.Select(choices=year_dict.items(),attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
