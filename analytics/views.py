98403d587381699e318bc3a0c496622cdebb3cb3
krishnateja <krishnateja.kaspe001@umb.edu>
2023-04-24 16:47:25 -0400

    three new graphs

--- analytics/views.py
+++ analytics/views.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 
 state_abv_ ='ma'
-dashboard_filters={'state_abv':'sca','survey_taken_year':2021}
+dashboard_filters={'state_abv':'sca','survey_taken_year':2022}
 
 def dropdown(request):
     global state_abv_
@@ -146,7 +146,7 @@ def index(request):
         layout = dict(
             title='school_surveys',
             yaxis = dict(range=[min(survey_true), max(survey_true)]),
-            barmode='group'
+            barmode='group',
         )
 
         fig = go.Figure(data=trace, layout=layout)
@@ -231,6 +231,9 @@ def index(request):
         'plot2': core_experience(),
         'plot3':implementation_level(),
         'plot4':core_experience_yearly(),
+        'plot5':implement_unified_sport_activity(),
+        'plot6':implement_youth_leadership_activity(),
+        'plot7':implement_school_engagement_activity(),
         'form':Filters()
     }
 
@@ -238,11 +241,11 @@ def index(request):
     
         
 def tables(request):
-
+    '''gives filters for queries after removing values which will not be there in table'''
     def filter_set():
         filters = []
         for key,val in dashboard_filters.items():
-            if val != 'all':
+            if val != 'all': #avoiding all beacuse all is not a value in table 
                 filters.append((key,val))
         filters = dict(filters)
         return filters
@@ -404,68 +407,126 @@ def tables(request):
     return render(request,'analytics/tables.html',context)
 
 
+'''
+Utility functions
+'''
 
-#class test_index():
-
-    def __init__(self) -> None:
-        super().__init__()
+'''
+input list or single dict 
+return list of percent values or returns nested dict with value and percent
+op sample :'state': {None: {'value': 0, 'percent_val': 0.0}, 'Yes': {'value': 4, 'percent_val': 7.0}, 'No': {'value': 53, 'percent_val': 93.0}}}
+'''
+def percentage_values(total_values):
+    if type(total_values) == list:
+        values = total_values
+        return [round(((i/sum(values))*100),2) for i in values]
     
-    def core_experience_data(self,key,state_abv_,survey_year=None):
-        select_names = {'sports':'unified_sports_component',
-                        'leadership':'youth_leadership_component',
-                        'whole_school':'whole_school_component'}
-        if key:
-            data = dict(SchoolDetails.objects.values_list(select_names[key]).filter(state_abv=state_abv_,survey_taken_year=survey_year).annotate(total = Count(select_names[key])))
-            data = data.get(True,0) #considering only partipated people
-            return data
-        else:
-            return 0
-        
-    def survey_years(self):
-        survey_years = SchoolDetails.objects.values_list('survey_taken_year',flat=True).distinct().order_by('survey_taken_year')
-        
-        return list(survey_years)
+    elif type(total_values) == dict:
+        data = list(total_values.values())
 
-    def core_experience_year_data(self,state_abv_):
-        survey_year= self.survey_years()
-        response = {'sports':[],'leadership':[],'whole_school':[],'survey_year':self.survey_year}
-        for year in survey_year:
-            response['sports'].append(self.core_experience_data('sports',state_abv_,survey_year=year))
-            response['leadership'].append(self.core_experience_data('leadership',state_abv_,survey_year=year))
-            response['whole_school'].append(self.core_experience_data('whole_school',state_abv_,survey_year=year))
+        try:
+            percent_arr=[round(((i/sum(data))*100),1) for i in data ] 
+        except ZeroDivisionError:
+            percent_arr = [0]*len(data)
+        res = {key:{'value':total_values[key],'percent_val':percent_arr[i]} for i,key in enumerate(total_values.keys()) }
+        return res
 
-        return response
-        
-    def core_experience_yearly(self,state_abv_):
-        data = self.core_experience_year_data(state_abv_)
+def filter_set():
+    filters = []
+    for key,val in dashboard_filters.items():
+        if val != 'all':
+            filters.append((key,val))
+    filters = dict(filters)
+    return filters
+    
+'''
+query function for below six graphs
+as pattern is same only column names are changing i used this method for fetching data
+#none/null values are by default counted as o in this query
+op sample:# {'sports_sports_teams': {'national': {'No': 2094, None: 0, 'Yes': 2033}, 'state': {None: 0, 'No': 24, 'Yes': 33}}, 'sports_unified_PE': {'national': {'No': 1945, None: 0, 'Yes': 2182}, 'state': {None: 0, 'No': 22, 'Yes': 35}}, 'sports_unified_fitness': {'national': {'No': 3292, None: 0, 'Yes': 835}, 'state': {None: 0, 'No': 44, 'Yes': 13}}, 'sports_unified_esports': {'national': {'No': 3903, None: 0, 'Yes': 224}, 'state': {None: 0, 'Yes': 4, 'No': 53}}, 'sports_young_athletes': {'national': {'No': 3450, None: 0, 'Yes': 677}, 'state': {None: 0, 'No': 46, 'Yes': 11}}, 'sports_unified_developmental_sports': {'national': {'No': 3505, None: 0, 'Yes': 622}, 'state': {None: 0, 'No': 49, 'Yes': 8}}}
 
-        print("CORE_EXP_YEAR:",data)
-        df = pd.DataFrame(data)
-        fig = px.line(df, x=df['survey_year'], y=df['sports'], labels={"survey_year":"year","sports":"core experience"})
-        fig.add_scatter(x=df['survey_year'],y=df['sports'], name="Unified sports")
-        fig.add_scatter(x=df['survey_year'],y=df['leadership'], name="Inclusive youth leadership")
-        fig.add_scatter(x=df['survey_year'],y=df['whole_school'], name="Whole school engagement")
-        title_name = "Core experience implementation over time in {0}".format(state_abv_)
-
-        fig.update_layout( 
-        legend=dict(
-            title=title_name, orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
-        ),
-        xaxis = dict (
-            tickmode='linear',
-            tick0 = min(df['survey_year']),
-            dtick=1
-        ) )
-        plot_div = plot(fig, output_type='div', include_plotlyjs=False)
-
-        return plot_div     
+'''
+def main_query(column_name,filters,key): 
+    if key=='state':
+        filters=filters
+    if key=='all':
+        filters=filters.copy()
+        filters.pop('state_abv')
+        print(filters)
+    data = dict(SchoolDetails.objects.values_list(column_name).filter(**filters).annotate(total = Count(column_name)))
+    print(data)
+    #test query to run in terminal: dict(SchoolDetails.objects.values_list('sports_sports_teams').filter(state_abv='sca',survey_taken_year=2022).annotate(total = Count('sports_sports_teams')))
+    return data
 
 
-#def return_plot(request):
-    test = test_index(request)
-    context ={
-        'plot4':test.core_experience_yearly(self,state_abv_),
-        'form':Filters()
-    }
 
-    return render(request, 'analytics/welcome.html', context)
+'''
+LOGIC
+'''
+def implement_unified_sport_activity():
+    response={'sports_sports_teams':{}, 'sports_unified_PE':{},'sports_unified_fitness':{},
+             'sports_unified_esports':{},'sports_young_athletes':{},'sports_unified_developmental_sports':{}}
+    filters=filter_set()
+    for column_name in response.keys():
+        response[column_name]['national']=percentage_values(main_query(column_name,filters,key='all')) 
+        response[column_name]['state']=percentage_values(main_query(column_name,filters,key='state'))
+    y_axis = ['Unified Sports Teams', 'Unified PE', 'Unified fitness','unified esports', 'young athletes(participants)', 'Unified Developmental Sports']
+    title='Percentage of schools implementing each Unified Sports activity'
+    return horizontal_bar_graph(response,y_axis,title)
+
+def implement_youth_leadership_activity():
+    response = {'leadership_unified_inclusive_club':{},'leadership_youth_training':{},'leadership_athletes_volunteer':{},
+               'leadership_youth_summit':{},'leadership_activation_committe':{}}
+    filters=filter_set()
+    for column_name in response.keys():
+        response[column_name]['national']=percentage_values(main_query(column_name,filters,key='all')) 
+        response[column_name]['state']=percentage_values(main_query(column_name,filters,key='state'))
+    y_axis=['unifed/Inclusive Club','Inclusive Youth leadership Training/Class','Young Athletes Volunteers','Youth summit','Youth Activation Committee']
+    title='Percentage of schools implementing each Inclusive Youth Leadership activity'
+    return horizontal_bar_graph(response,y_axis,title)
+
+def implement_school_engagement_activity():
+    response = {'engagement_spread_word_campaign':{},'engagement_fansinstands':{},'engagement_sports_day':{},
+                'engagement_fundraisingevent':{},'engagement_SO_play':{},'engagement_fitness_challenge':{}}
+    
+    filters=filter_set()
+    for column_name in response.keys():
+        response[column_name]['national']=percentage_values(main_query(column_name,filters,key='all')) 
+        response[column_name]['state']=percentage_values(main_query(column_name,filters,key='state'))
+    y_axis=['Spread the word'+'<br>'+'Inclusion/Respect/Disabilityy' +'<br>'+'Awareness Campaign','Unified Sports pep Rally','Unified Sports Day/Festival','Fundraising events /activities','Special Olympics play/performance','Unified Fitness Challenge']
+    title='Percentage of schools implementing each Inclusive Whole School Engagement activity'
+    return horizontal_bar_graph(response,y_axis,title)
+
+def frequency_of_leadership():
+    return None
+'''
+CHARTS
+'''
+def horizontal_bar_graph(response,y_axis,heading):
+    print(response)
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        y=y_axis,
+        x=[response[val]['national'].get('Yes',{}).get('percent_val',0) for val in response if response],
+        name='National',
+        orientation='h',
+        marker=dict(
+            color='rgba(246, 78, 139, 0.6)',
+            line=dict(color='rgba(246, 78, 139, 1.0)', width=3)
+        )
+    ))
+    fig.add_trace(go.Bar(
+        y=y_axis,
+        x=[response[val]['state'].get('Yes',{}).get('percent_val',0) for val in response if response],
+        name='state',
+        orientation='h',
+        marker=dict(
+            color='rgba(58, 71, 80, 0.6)',
+            line=dict(color='rgba(58, 71, 80, 1.0)', width=3)
+        )
+    ))
+
+    fig.update_layout(title=heading,barmode='group',xaxis_range=[0,100])
+    plot_div = plot(fig, output_type='div', include_plotlyjs=False)
+    return plot_div
+    
